77684bcf4a54b601c93bc23c758c1ee648f467b3
Commander-Ayush <ayushkrjha221004@gmail.com>
2026-01-19 23:24:43 +0530

    CategoryController Understood

--- src/main/java/com/udemy/sbecom/controller/CategoryController.java
+++ src/main/java/com/udemy/sbecom/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.udemy.sbecom.controller;
 
 import com.udemy.sbecom.model.Category;
+import com.udemy.sbecom.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +14,21 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
+    private final CategoryService categoryService;
 
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
+    @GetMapping("/api/public/categories")
+    public List<Category> getCategories(){
+        return categoryService.getCategories();
+    }
+
+    @PostMapping("/api/public/categories")
+    public String addCategory(@RequestBody Category category){
+        categoryService.createCategory(category);
+        return "Category Created";
+    }
 
 }
--- src/main/java/com/udemy/sbecom/service/CategoryServiceImpl.java
+++ src/main/java/com/udemy/sbecom/service/CategoryServiceImpl.java
@@ -1,10 +1,11 @@
 package com.udemy.sbecom.service;
 
 import com.udemy.sbecom.model.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     List<Category> categories = new ArrayList<>();
